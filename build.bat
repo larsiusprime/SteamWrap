55e777e5901f72b6d55f972d69343389f5864667
Martin Jonasson <grapefrukt@grapefrukt.com>
2013-11-05 12:30:30 +0100

    Test build script for Windows

---
+++ build.bat
@@ -0,0 +1,13 @@
+@echo off
+rem build native ndll
+cd native
+haxelib run hxcpp Build.xml
+
+rem build test exe
+cd ..
+cd steamwrap
+haxe -cp .. Test.hx -main Test -cpp ..\temp\hx
+
+cd ..
+copy temp\hx\Test.exe ndll\Windows
+copy native\lib\*.dll ndll\Windows
