f9eb79dbe7f86819a6fef774cba6c8ba5cd5a37e
Samu Aapa <samu@sharkpunch.com>
2015-08-17 10:20:14 +0300

    Removed the commented out "cd .." command that caused running the batch file to end up in a wrong folder than where it was ran from.

--- build.bat
+++ build.bat
@@ -4,10 +4,11 @@ cd native
 haxelib run hxcpp Build.xml
 
 rem build test exe
-rem cd ..
+cd ..
 rem cd steamwrap
 rem haxe -cp .. Test.hx -main Test -cpp ..\temp\hx
 
 rem cd ..
 rem copy temp\hx\Test.exe ndll\Windows
 rem copy native\lib\*.dll ndll\Windows
+
