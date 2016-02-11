2288416a797f7c1c4d26d635b7fb9b4412061421
Lars Doucet <lars.doucet@gmail.com>
2016-02-11 15:25:25 -0600

    Fix setup.bat

--- setup.bat
+++ setup.bat
@@ -1,8 +1,11 @@
 @echo off
-set /p STEAMPATH=Enter path to Steam SDK root:
+set /p STEAMPATH=Enter path to Steam SDK root (folder that contains "\redistributable_bin", no trailing slash):
+set /p HXCPP=Enter path to hxccp root (folder that contains "\include", no trailing slash):
 copy "%STEAMPATH%\public\steam\*.h" native\include\steam\
 copy "%STEAMPATH%\redistributable_bin\steam_api.dll" native\lib\win32\
 copy "%STEAMPATH%\redistributable_bin\steam_api.lib" native\lib\win32\
-copy "%STEAMPATH%\redistributable_bin\osx32\libsteam_api.dylib" native\lib\
-copy "%HAXEPATH%lib\hxcpp\3,2,205\include\hx\*.h" native\include\hx
+copy "%STEAMPATH%\redistributable_bin\osx32\libsteam_api.dylib" native\lib\osx64
+copy "%STEAMPATH%\redistributable_bin\linux32\libsteam_api.so" native\lib\linux32
+copy "%STEAMPATH%\redistributable_bin\linux64\libsteam_api.so" native\lib\linux64
+copy "%HXCPP%\include\hx\*.h" native\include\hx
 pause
