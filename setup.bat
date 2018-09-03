1523e9c89bb29102cb926c9a8daf75fc89907459
Lars Doucet <lars.doucet@gmail.com>
2018-09-03 12:50:40 -0500

    fix setup.bat to copy 64-bit windows libs from steamworks

--- setup.bat
+++ setup.bat
@@ -4,8 +4,10 @@ set /p HXCPP=Enter path to hxccp root (folder that contains "\include", no trail
 copy "%STEAMPATH%\public\steam\*.h" native\include\steam\
 copy "%STEAMPATH%\redistributable_bin\steam_api.dll" native\lib\win32\
 copy "%STEAMPATH%\redistributable_bin\steam_api.lib" native\lib\win32\
-copy "%STEAMPATH%\redistributable_bin\osx32\libsteam_api.dylib" native\lib\osx64
-copy "%STEAMPATH%\redistributable_bin\linux32\libsteam_api.so" native\lib\linux32
-copy "%STEAMPATH%\redistributable_bin\linux64\libsteam_api.so" native\lib\linux64
+copy "%STEAMPATH%\redistributable_bin\win64\steam_api64.dll" native\lib\win64\
+copy "%STEAMPATH%\redistributable_bin\win64\steam_api64.lib" native\lib\win64\
+copy "%STEAMPATH%\redistributable_bin\osx32\libsteam_api.dylib" native\lib\osx64\
+copy "%STEAMPATH%\redistributable_bin\linux32\libsteam_api.so" native\lib\linux32\
+copy "%STEAMPATH%\redistributable_bin\linux64\libsteam_api.so" native\lib\linux64\
 copy "%HXCPP%\include\hx\*.h" native\include\hx
 pause
