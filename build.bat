a325943c31a222854d449031971ff485b757f609
Lars Doucet <lars.doucet@gmail.com>
2018-09-03 13:40:47 -0500

    add 64-bit windows support

--- build.bat
+++ build.bat
@@ -10,6 +10,18 @@ cd ..
 
 copy native\lib\win32\*.dll ndll\Windows
 
+
+:: build the 64-bit native ndll
+
+cd native
+
+haxelib run hxcpp Build.xml -DHXCPP_M64
+
+cd ..
+
+copy native\lib\win64\*.dll ndll\Windows64
+
+
 :: build the example program "Test.exe"
 
 cd example
@@ -18,3 +30,8 @@ cd ..
 
 copy temp\hx\Test.exe ndll\Windows
 
+cd example
+haxe -cp .. Test.hx -main Test -cpp ..\temp64\hx -D HXCPP_M64=1
+cd ..
+
+copy temp64\hx\Test.exe ndll\Windows64
