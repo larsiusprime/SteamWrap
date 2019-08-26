b14aea251957610fa4218c70c0a425d25fa3431a
Lars Doucet <lars.doucet@gmail.com>
2019-08-26 08:39:25 -0500

    fix setup script for mac

--- setup.bat
+++ setup.bat
@@ -6,7 +6,7 @@ copy "%STEAMPATH%\redistributable_bin\steam_api.dll" native\lib\win32\
 copy "%STEAMPATH%\redistributable_bin\steam_api.lib" native\lib\win32\
 copy "%STEAMPATH%\redistributable_bin\win64\steam_api64.dll" native\lib\win64\
 copy "%STEAMPATH%\redistributable_bin\win64\steam_api64.lib" native\lib\win64\
-copy "%STEAMPATH%\redistributable_bin\osx32\libsteam_api.dylib" native\lib\osx64\
+copy "%STEAMPATH%\redistributable_bin\osx\libsteam_api.dylib" native\lib\osx64\
 copy "%STEAMPATH%\redistributable_bin\linux32\libsteam_api.so" native\lib\linux32\
 copy "%STEAMPATH%\redistributable_bin\linux64\libsteam_api.so" native\lib\linux64\
 copy "%HXCPP%\include\hx\*.h" native\include\hx
--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ read HXCPP
 cp "$STEAMPATH"/public/steam/*.h native/include/steam
 cp "$STEAMPATH"/redistributable_bin/steam_api.dll native/lib/win32 > /dev/null
 cp "$STEAMPATH"/redistributable_bin/steam_api.lib native/lib/win32 > /dev/null
-cp "$STEAMPATH"/redistributable_bin/osx32/libsteam_api.dylib native/lib/osx64 > /dev/null
+cp "$STEAMPATH"/redistributable_bin/osx/libsteam_api.dylib native/lib/osx64 > /dev/null
 cp "$STEAMPATH"/redistributable_bin/linux32/libsteam_api.so native/lib/linux32 > /dev/null
 cp "$STEAMPATH"/redistributable_bin/linux64/libsteam_api.so native/lib/linux64 > /dev/null
 cp "$HXCPP"/include/hx/*.h native/include/hx > /dev/null
